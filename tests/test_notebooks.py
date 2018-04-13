2f8cc060087779848fce031e3fb7838143054b13
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-13 10:32:05 -0700

    a few more integers to take care of, randomly ignore 3 of the notebooks in each test to reduce travis time

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,15 +1,19 @@
 import os
+import numpy as np
 import testipynb
 import unittest
 
 NBDIR = os.path.sep.join(
     os.path.abspath(__file__).split(os.path.sep)[:-2] + ['notebooks']
 )
+n_ignore = 3  # so we don't run over-time on travis, randomly ignore 3 notebooks
 
 class TestNotebooks(unittest.TestCase):
 
     def test_notebooks(self):
         Test = testipynb.TestNotebooks(directory=NBDIR, timeout=600)
+        ignore_inds = np.random.choice(len(Test._nbnames), n_ignore)
+        Test.ignore = [Test._nbnames[i] for i in ignore_inds]
         self.assertTrue(Test.run_tests())
 
 if __name__ == "__main__":
