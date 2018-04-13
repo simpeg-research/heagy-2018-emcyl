164b0ef36714fa1aa0dffcf7ce16c25297fe5e66
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-13 10:07:40 -0700

    update how we work with the output of the analytics to overcome vectormath challenges that will be addressed in https://github.com/seequent/vectormath/pull/27

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -5,12 +5,11 @@ import unittest
 NBDIR = os.path.sep.join(
     os.path.abspath(__file__).split(os.path.sep)[:-2] + ['notebooks']
 )
-# IGNORE = ["TEM_VerticalConductor_1D_stiched_invrsion"]
 
 class TestNotebooks(unittest.TestCase):
 
     def test_notebooks(self):
-        Test = testipynb.TestNotebooks(directory=NBDIR, timeout=1800)
+        Test = testipynb.TestNotebooks(directory=NBDIR, timeout=600)
         self.assertTrue(Test.run_tests())
 
 if __name__ == "__main__":
