49844466ec15c9d617e831edd1a988cceec84b8b
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-14 10:23:15 -0700

    just ignore notebook 5

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,11 +6,13 @@ import unittest
 NBDIR = os.path.sep.join(
     os.path.abspath(__file__).split(os.path.sep)[:-2] + ['notebooks']
 )
-n_ignore = 6  # so we don't run over-time on travis, randomly ignore 3 notebooks
 
+IGNORE = ["5_FDEM_following_Augustin_Fig3"]
+
+# n_ignore = 2  # so we don't run over-time on travis, randomly ignore 3 notebooks
 Test = testipynb.TestNotebooks(directory=NBDIR, timeout=2100)
-ignore_inds = np.random.choice(len(Test._nbnames), n_ignore)
-Test.ignore = [Test._nbnames[i] for i in ignore_inds]
+# ignore_inds = np.random.choice(len(Test._nbnames), n_ignore)
+Test.ignore = IGNORE # set(IGNORE + [Test._nbnames[i] for i in ignore_inds])
 TestNotebooks = Test.get_tests()
 
 if __name__ == "__main__":
