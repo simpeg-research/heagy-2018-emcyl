9bfb8d4edb73d4b73d913ae1167579aec0fbf255
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-14 08:41:05 -0700

    test one notebook

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,9 +6,9 @@ import unittest
 NBDIR = os.path.sep.join(
     os.path.abspath(__file__).split(os.path.sep)[:-2] + ['notebooks']
 )
-n_ignore = 5  # so we don't run over-time on travis, randomly ignore 3 notebooks
+n_ignore = 6  # so we don't run over-time on travis, randomly ignore 3 notebooks
 
-Test = testipynb.TestNotebooks(directory=NBDIR, timeout=1800)
+Test = testipynb.TestNotebooks(directory=NBDIR, timeout=2100)
 ignore_inds = np.random.choice(len(Test._nbnames), n_ignore)
 Test.ignore = [Test._nbnames[i] for i in ignore_inds]
 TestNotebooks = Test.get_tests()
