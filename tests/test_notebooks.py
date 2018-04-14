1e5c3f2887641a3070a09c121c225b4a3128adf0
Lindsey Heagy <lindseyheagy@gmail.com>
2018-04-14 11:11:33 -0700

    remove radial labels on the plot showing the mesh for the commer comparison, test at most 5 notebooks from the batch

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -9,9 +9,9 @@ NBDIR = os.path.sep.join(
 
 IGNORE = ["5_FDEM_following_Augustin_Fig3"]
 
-# n_ignore = 2  # so we don't run over-time on travis, randomly ignore 3 notebooks
+n_ignore = 2  # so we don't run over-time on travis, randomly ignore 3 notebooks
 Test = testipynb.TestNotebooks(directory=NBDIR, timeout=2100)
-# ignore_inds = np.random.choice(len(Test._nbnames), n_ignore)
+ignore_inds = np.random.choice(len(Test._nbnames), n_ignore)
 Test.ignore = IGNORE # set(IGNORE + [Test._nbnames[i] for i in ignore_inds])
 TestNotebooks = Test.get_tests()
 
